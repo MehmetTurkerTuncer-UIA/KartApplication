a3b951130d94220c254b47ca30baa185ee79c6be
amtkrc <ahmetki@uia.no>
2024-10-22 03:35:01 +0200

    Home OpprettSak (kart) Oppdatert

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,4 +32,86 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+
+      private static AreaChange lastChange = null;
+
+    // GET: Bruker/Index formunu görüntüle
+    [HttpGet]
+    public IActionResult OpprettSak()
+    {
+        return View();
+    }
+
+    // POST: Kullanıcıdan gelen GeoJson, adres ve açıklamayı işleme al
+    [HttpPost]
+    public IActionResult OpprettSak(string geoJson, string description, string address)
+    {
+        // Yeni 8 haneli numeric ID oluştur
+        string newId = IdGenerator.GenerateNumericIdFromGuid();
+
+        // AreaChange nesnesini doldur
+        lastChange = new AreaChange
+        {
+            Id = newId,
+            GeoJson = geoJson,
+            Description = description,
+            Address = address,
+            Dato = DateTime.Now // Tarih burada atanıyor
+        };
+
+        // Kayıt sonrası beskrivelse sayfasına yönlendir
+        return RedirectToAction("Beskrivelse");
+    }
+
+    // GET: Bruker/Beskrivelse sayfasını göster
+    [HttpGet]
+    public IActionResult Beskrivelse()
+    {
+        return View(lastChange);
+    }
+
+    // POST: Beskrivelse sayfasındaki açıklamayı işleyip Oversikt'e gönder
+    [HttpPost]
+    public IActionResult Beskrivelse(string description)
+    {
+        if (lastChange != null)
+        {
+            lastChange.Description = description;
+        }
+        return RedirectToAction("Oversikt");
+    }
+
+    // GET: Bruker/Oversikt sayfasını görüntüle
+    [HttpGet]
+    public IActionResult Oversikt()
+    {
+        return View(lastChange);
+    }
+
+    // GET: Bruker/Kvittering - Referans numarasını kullanarak kaydı göster
+    [HttpGet]
+    public IActionResult Kvittering(string id)
+    {
+        if (lastChange != null && lastChange.Id == id)
+        {
+            return View(lastChange); // lastChange dinamik verilerle doluysa göster
+        }
+
+        return NotFound(); // Eğer lastChange bulunamazsa hata göster
+    }
+
+    // POST: Bruker/Kvittering sayfasına yönlendir
+    [HttpPost]
+    public IActionResult Kvittering()
+    {
+        if (lastChange != null)
+        {
+            return View(lastChange); // Kaydedilen başvuruyu göster
+        }
+
+        return NotFound(); // Eğer lastChange boşsa hata göster
+    }
+
+    
 }
