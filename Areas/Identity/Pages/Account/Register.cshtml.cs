06fc86faa4293b83dffb34788443105d7fbbef2a
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2024-10-31 19:06:50 +0100

    every users will register like who have a bruker role

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using KartApplication.Data;
 using KartApplication.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
@@ -30,13 +31,15 @@ namespace KartApplication.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<ApplicationUser> _emailStore;   //  IdentityUser  ApplicationUser yapilmadi !!!
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public RegisterModel(
             UserManager<ApplicationUser> userManager,
             IUserStore<ApplicationUser> userStore,
             SignInManager<ApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -44,6 +47,7 @@ namespace KartApplication.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager;
         }
 
         /// <summary>
@@ -134,6 +138,13 @@ namespace KartApplication.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+                    if (!await _roleManager.RoleExistsAsync(UserRoles.Role_Bruker))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(UserRoles.Role_Bruker));
+                    }
+                    await _userManager.AddToRoleAsync(user, UserRoles.Role_Bruker);
+
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
