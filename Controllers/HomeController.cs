3daecf5f0608d19e55d3c3b92e2e4ac7186c1135
Q4115-Gökhan Güzeler <131613027+GkhnGzlr@users.noreply.github.com>
2024-11-23 20:51:27 +0100

    Metode for å slette midlertidige registreringer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -137,7 +137,7 @@ namespace KartApplication.Controllers
             return View(model);
         }
 
-        // Geçici Kayıtları Silme Metodu (isteğe bağlı zamanlayıcı ile kullanılabilir)
+        // Metode for å slette midlertidige registreringer (kan brukes med en valgfri tidsplan).
         public async Task DeleteTemporaryRecords()
         {
             var temporaryRecords = await _context.SakModels
